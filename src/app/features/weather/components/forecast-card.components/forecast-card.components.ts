64d4e5d1a0cfdbe1235577c3b155ba6e669f8568
noah <noahvf16@outlook.com>
2026-03-02 21:38:06 -0300

    feat: add WeatherCardComponents with HTML, CSS, and test files

---
+++ src/app/features/weather/components/forecast-card.components/forecast-card.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-card.components',
+  imports: [],
+  templateUrl: './forecast-card.components.html',
+  styleUrl: './forecast-card.components.css',
+})
+export class WeatherCardComponents {
+
+}
