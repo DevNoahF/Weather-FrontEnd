6beb49bc286a1a7eadfbf0958c2865a35d7a4387
noah <noahvf16@outlook.com>
2026-03-03 15:02:59 -0300

    feat: implement forecast card component with weather data display

--- src/app/features/weather/components/forecast-card.components/forecast-card.components.ts
+++ src/app/features/weather/components/forecast-card.components/forecast-card.components.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { weatherResponse } from '../../../../core/models/weatherResponse';
 
 @Component({
-  selector: 'app-forecast-card.components',
+  selector: 'app-forecast-card',
   imports: [],
   templateUrl: './forecast-card.components.html',
   styleUrl: './forecast-card.components.css',
 })
-export class WeatherCardComponents {
-
+export class ForecastCardComponent {
+  response = input<weatherResponse | null>(null);
+  loading = input<boolean>(false);
 }
