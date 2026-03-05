5ee8912d95370b31c93f215745c012dd58792039
DevNoahF <noahvf16@outlook.com>
2026-03-04 23:27:43 -0300

    feat: update forecast card component to use daysWithIcon for weather data and include icon URLs

--- src/app/features/weather/components/forecast-card.components/forecast-card.components.ts
+++ src/app/features/weather/components/forecast-card.components/forecast-card.components.ts
@@ -1,13 +1,26 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { weatherResponse } from '../../../../core/models/weatherResponse';
+import getWeatherIcon from '../../../../core/config/weather-icons.config';
 
 @Component({
   selector: 'app-forecast-card',
   imports: [],
   templateUrl: './forecast-card.components.html',
   styleUrl: './forecast-card.components.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ForecastCardComponent {
   response = input<weatherResponse | null>(null);
   loading = input<boolean>(false);
+
+  private readonly maxDaysToShow = 7;
+
+  daysWithIcon = computed(() => {
+    const res = this.response();
+    if (!res) return [];
+    return res.days.slice(0, this.maxDaysToShow).map((day) => ({
+      ...day,
+      iconUrl: getWeatherIcon(day.icon),
+    }));
+  });
 }
